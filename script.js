d8c832d11d8a648b6c64bc9c417b96d71996fd01
Spencer Williams <spilliams@gmail.com>
2022-08-07 16:14:24 -0500

    Add clear and backspace functionality

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+// create variable to store user input
+let inputNumber = "";
+// variables to store subsequent input and operator
+let firstNumber = "";
+let secondNumber = "";
+let currentOperator = "";
+
 // declare basic calculator functionality
-function add(a, b) {
+function add(a, b) { 
     return a + b;
 }
 function subtract(a, b) {
@@ -12,22 +19,62 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(a, b) {
-
-}
-
-// return the value of the button
+// add the value input into a string 
 function buttonValue(click) {
-    click.stopPropogation();
-    console.log(this.id); 
+    click.stopPropagation();
+    inputNumber += this.id;
+    updateCalculatorDisplay();
 }
+// set the current operator
+function operatorValue(click) {
+    click.stopPropagation();
+    currentOperator = this.id;
+    firstNumber = inputNumber;
+    inputNumber = "";
+}
+// perform the actual operation
+function operate(a, b, operator) {
+    let first = parseInt(a, 10);yt
+    let second = parseInt(b, 10);
+
+    if (operator === 'add') {
+        add(first, second);
+        updateCalculatorDisplay();
+    }
+
+}
+
+// clear the calculator screen
+function clear() {
+    inputNumber = "";
+    updateCalculatorDisplay();
+}
+
+function backspace() {
+    let tempString = "";
+    tempString = inputNumber.slice(0, -1);
+    inputNumber = tempString;
+    updateCalculatorDisplay();
+}
+
 // update the current number
 function updateCalculatorDisplay() {
     const calculatorDisplay = document.querySelector('.lowerDisplay');
-    calculatorDisplay.textContent = `123`;
+    calculatorDisplay.textContent = `${inputNumber}`;
 }
-// get the input for the calculator
-const calculatorButtons = document.querySelectorAll('button');
+
+// get the number input for the calculator
+const calculatorButtons = document.querySelectorAll('.numberButton');
 calculatorButtons.forEach(button => button.addEventListener('click', buttonValue, {capture: false}));
 
-updateCalculatorDisplay();
+// get the operator
+const calculatorOperator = document.querySelectorAll('.operatorButton');
+calculatorButtons.forEach(button => button.addEventListener('click', buttonValue, {capture: false}));
+
+// clear button functionality
+const calculatorClear = document.querySelector('.clear');
+calculatorClear.addEventListener('click', clear, {capture: false});
+
+// backspace button functionality
+const calculatorBackspace = document.querySelector('#backspace');
+calculatorBackspace.addEventListener('click', backspace, {capture: false});
